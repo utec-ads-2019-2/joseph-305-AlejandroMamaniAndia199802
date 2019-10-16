0e0adb69e1343f06618a5065cc2ad12c1a4adacc
Alejandro1998elebebecito <49255376+Alejandro1998elebebecito@users.noreply.github.com>
2019-10-16 17:37:05 -0500

    Add content

--- main.cpp
+++ main.cpp
@@ -4,62 +4,61 @@
 #include <cstring>
 using namespace std;
 
-#ifdef ONLINE_JUDGE
-int get_random_number(int k)
+double get_random_number(int A)
 {
-	const int numbers[] = {0, 2, 7, 5, 30, 169, 441, 1872, 7632, 1740, 93313, 459901, 1358657, 2504881};
-	return numbers[k];
-}
-#else
-long long get_random_number(int k)
-{
-    const int k_max = 13;
-    bool executed[k_max * 2 + 1];
-    int n = k * 2;
-    for (long long m = k + 1; ; m++)
+    const int max = 13;
+    bool executed[max * 2 + 1];
+    int n = A * 2;
+    for (double m = A + 1; ; m++)
     {
-        int nr_executed = 0;
+        int n_executed = 0;
         memset(executed, 0, sizeof(executed));
         int i = 1;
         while (true) {
-            long long j = m;
-            while (true) {
+            double j = m;
+            while (true)
+            {
                 if (!executed[i] && !--j)
                     break;
                 if (++i > n)
                     i = 1;
             }
+
 #ifdef DEBUG
             cout << i << ' ';
 #endif
-            executed[i] = true;
-            if (i > k) {
-                if (++nr_executed == k) {
-#ifdef DEBUG
-                    cout << endl;
-#endif
-                    return m;
-                }
-            }
-            else {
-#ifdef DEBUG
-                cout << endl;
-#endif
-                break;
-            }
-        }
-    }
-}
-#endif
 
+            executed[i] = true;
+            if (i > A)
+            {
+                if (++n_executed == A)
+                {
+
+#ifdef DEBUG
+                   cout << endl;
+#endif
+                   return m;
+               }
+           }
+           else {
+
+ #ifdef DEBUG
+                cout << endl;
+ #endif
+
+               break;
+           }
+       }
+   }
+}
 int main()
 {
-    while (true) {
-        int k;
-        cin >> k;
-        if (!k)
-            break;
-        cout << get_random_number(k) << endl;
-    }
-    return 0;
+   while (true) {
+       int A;
+       cin >> A;
+       if (!A)
+           break;
+       cout << get_random_number(A) << endl;
+   }
+   return 0;
 }

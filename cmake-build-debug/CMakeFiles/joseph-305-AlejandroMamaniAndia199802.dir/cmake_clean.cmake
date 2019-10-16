f81a9eec4d712623908c73601a50a65114d6d48c
Alejandro1998elebebecito <49255376+Alejandro1998elebebecito@users.noreply.github.com>
2019-10-16 17:17:34 -0500

    Add content

---
+++ cmake-build-debug/CMakeFiles/joseph-305-AlejandroMamaniAndia199802.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/joseph-305-AlejandroMamaniAndia199802.dir/main.cpp.obj"
+  "joseph-305-AlejandroMamaniAndia199802.pdb"
+  "joseph-305-AlejandroMamaniAndia199802.exe"
+  "joseph-305-AlejandroMamaniAndia199802.exe.manifest"
+  "libjoseph-305-AlejandroMamaniAndia199802.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/joseph-305-AlejandroMamaniAndia199802.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
